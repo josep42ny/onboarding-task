89758f2490a606cb9e819b140fca54f2452877e1
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-05-26 16:21:04 +0200

    feat: add heroes service

---
+++ src/app/interfaces/hero.ts
@@ -0,0 +1,8 @@
+export interface Hero {
+
+  name: String;
+  description: String;
+  location: String;
+  habilities: String[];
+
+}
---
+++ src/app/services/heroes.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Hero } from '../interfaces/hero';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+  private apiServerUrl: String = 'http://localhost:3000'
+  private http: HttpClient = inject(HttpClient);
+
+  public getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.apiServerUrl}/hero`);
+  }
+
+  public getHero(heroId: number): Observable<Hero> {
+    return this.http.get<Hero>(`${this.apiServerUrl}/hero/${heroId}`);
+  }
+
+  public updateHero(hero: Hero): Observable<Hero> {
+    return this.http.put<Hero>(`${this.apiServerUrl}/hero/`, hero);
+  }
+
+  public deleteHero(heroId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/hero/${heroId}`);
+  }
+
+}
