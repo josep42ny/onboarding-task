dd579566b76fbd7b92817d716584cdb1a2c2ce5d
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-05-28 15:15:38 +0200

    feat: add footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
