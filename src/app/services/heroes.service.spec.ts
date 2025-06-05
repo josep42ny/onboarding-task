78c306d1910f5089800ba6222a54c1110553560c
josep42ny <82282886+josep42ny@users.noreply.github.com>
2025-06-05 11:53:18 +0200

    feat: test heroes service

--- src/app/services/heroes.service.spec.ts
+++ src/app/services/heroes.service.spec.ts
@@ -2,19 +2,31 @@ import type { Hero } from '../interfaces/hero';
 import { TestBed } from '@angular/core/testing';
 import { HeroesService } from './heroes.service';
 import { HttpErrorResponse, HttpEventType, provideHttpClient } from '@angular/common/http';
-import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting, TestRequest } from '@angular/common/http/testing';
 import { environment } from '../../environments/environment.development';
+import { config, firstValueFrom, Observable } from 'rxjs';
 
-const EXPECTED_HERO = {
-  "id": 0,
+interface HeroesParams<T> {
+  params: Array<any>,
+  url: string,
+  method: string,
+}
+
+const MOCK_HERO: Hero = {
+  "id": 420,
   "name": "Captain Blaze",
-  "description": "A fearless hero who commands the power of fire and heat.",
+  "description": "A fearless hero who commands the power of fire and smoke.",
   "location": "Volcanic Ridge",
   "powers": "Pyrokinesis",
-  "imageUrl": "https://picsum.photos/seed/1/400/300"
+  "imageUrl": "https://picsum.photos/seed/420/400/300"
 };
-const EXPECTED_HEROES = [EXPECTED_HERO];
-const URL = environment.apiBaseUrl + '/hero?name_like=';
+const MOCK_HEROES = [MOCK_HERO, MOCK_HERO];
+const HERO_REQ_ID = 0;
+const GET_HERO_REQ = `${environment.apiBaseUrl}/hero/${HERO_REQ_ID}`;
+const GET_HEROES_REQ = `${environment.apiBaseUrl}/hero?name_like=`;
+const ADD_HERO_REQ = `${environment.apiBaseUrl}/hero`;
+const UPDATE_HERO_REQ = `${environment.apiBaseUrl}/hero/${MOCK_HERO.id}`;
+const DELETE_HERO_REQ = `${environment.apiBaseUrl}/hero/${MOCK_HERO.id}`;
 
 describe('HeroesService', () => {
   let service: HeroesService;
@@ -40,18 +52,83 @@ describe('HeroesService', () => {
     expect(service).toBeTruthy();
   });
 
-  it('gets heroes', () => {
-    service.getHeroes().subscribe((heroes: Hero[]) => {
-      expect(heroes).toEqual(EXPECTED_HEROES);
+  it('gets a single hero', () => {
+    const heores: Observable<Hero> = service.getHero(HERO_REQ_ID);
+    const heroPromise: Promise<Hero> = firstValueFrom(heores);
+
+    //capture the request
+    const req: TestRequest = httpTesting.expectOne({
+      method: 'GET',
+      url: GET_HERO_REQ,
+    }, 'Request to load hero');
+    req.flush(MOCK_HERO); //mock the response
+
+    heroPromise.then((hero: Hero) => {
+      expect(hero).toEqual(MOCK_HERO);
     });
-
-    const request = httpTesting.expectOne(URL);
-    request.flush(EXPECTED_HEROES);
   });
 
-  it('fails to get heroes', () => {
-    const request = httpTesting.expectOne(URL);
-    request.error(new ProgressEvent('Internal Server Error'), { status: 500 });
+  it('gets multiple heroes', () => {
+    const heores: Observable<Hero[]> = service.getHeroes();
+    const heroPromise: Promise<Hero[]> = firstValueFrom(heores);
+
+    //capture the request
+    const req: TestRequest = httpTesting.expectOne({
+      method: 'GET',
+      url: GET_HEROES_REQ,
+    }, 'Request to load heroes');
+    req.flush(MOCK_HEROES); //mock the response
+
+    heroPromise.then((hero: Hero[]) => {
+      expect(hero).toEqual(MOCK_HEROES);
+    });
   });
 
+  it('adds a hero', () => {
+    const heroes: Observable<Hero> = service.addHero(MOCK_HERO);
+    const heroPromise: Promise<Hero> = firstValueFrom(heroes);
+
+    //capture the request
+    const req: TestRequest = httpTesting.expectOne({
+      method: 'POST',
+      url: ADD_HERO_REQ,
+    }, 'Request to load heroes');
+    req.flush(MOCK_HERO); //mock the response
+
+    heroPromise.then((hero: Hero) => {
+      expect(hero).toEqual(MOCK_HERO);
+    });
+  });
+
+  it('updates a hero', () => {
+    const heroes: Observable<Hero> = service.updateHero(MOCK_HERO);
+    const heroPromise: Promise<Hero> = firstValueFrom(heroes);
+
+    //capture the request
+    const req: TestRequest = httpTesting.expectOne({
+      method: 'PUT',
+      url: UPDATE_HERO_REQ,
+    }, 'Request to load heroes');
+    req.flush(MOCK_HERO); //mock the response
+
+    heroPromise.then((hero: Hero) => {
+      expect(hero).toEqual(MOCK_HERO);
+    });
+  });
+
+  it('deletes a hero', () => {
+    const heroes: Observable<void> = service.deleteHero(MOCK_HERO.id);
+    const heroPromise: Promise<void> = firstValueFrom(heroes);
+
+    //capture the request
+    const req: TestRequest = httpTesting.expectOne({
+      method: 'DELETE',
+      url: DELETE_HERO_REQ,
+    }, 'Request to load heroes');
+    req.flush(null); //mock the response
+
+    heroPromise.then((response: void) => {
+      expect(response).toBe(null);
+    });
+  });
 });
